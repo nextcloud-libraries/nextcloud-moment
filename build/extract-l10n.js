6eb393db2fdd7d06eb485c7c05c94554165ac513
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-06-27 11:56:23 +0200

    Initial implementation
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ build/extract-l10n.js
@@ -0,0 +1,25 @@
+const { GettextExtractor, JsExtractors } = require('gettext-extractor');
+
+let extractor = new GettextExtractor();
+
+extractor
+    .createJsParser([
+        JsExtractors.callExpression('t', {
+            arguments: {
+                text: 0,
+                context: 1
+            }
+        }),
+        JsExtractors.callExpression('n', {
+            arguments: {
+                text: 1,
+                textPlural: 2,
+                context: 3
+            }
+        })
+    ])
+    .parseFilesGlob('./lib/**/*.@(ts|js)');
+
+extractor.savePotFile('./messages.pot');
+
+extractor.printStats();
---
+++ lib/index.ts
@@ -0,0 +1,23 @@
+import * as moment from 'moment'
+import { getLocale, translate } from 'nextcloud-l10n'
+
+function t(text: string): string {
+    return translate('nextcloud-moment', text)
+}
+
+moment.locale(getLocale())
+
+moment.updateLocale(
+    getLocale(),
+    {
+        parentLocale: getLocale(),
+        relativeTime: Object.assign(
+            // @ts-ignore
+            moment.localeData(getLocale())._relativeTime,
+            {
+                s: t('seconds ago'),
+            }
+        )
+    })
+
+export default moment
