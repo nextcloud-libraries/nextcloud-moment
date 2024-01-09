72a5dfabf703be4b585b3d058d1442366b84dfc4
Ferdinand Thiessen <opensource@fthiessen.de>
2024-01-10 00:05:21 +0100

    fix(lint): Fix linter errors
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- build/extract-l10n.js
+++ build/extract-l10n.js
@@ -1,18 +1,18 @@
-const { GettextExtractor, JsExtractors } = require('gettext-extractor');
+const { GettextExtractor, JsExtractors } = require('gettext-extractor')
 
-let extractor = new GettextExtractor();
+const extractor = new GettextExtractor()
 
 extractor
-    .createJsParser([
-        JsExtractors.callExpression('gt.gettext', {
-            arguments: {
-                text: 0,
-                context: 1
-            }
-        }),
-    ])
-    .parseFilesGlob('./lib/**/*.@(ts|js)');
+	.createJsParser([
+		JsExtractors.callExpression('gt.gettext', {
+			arguments: {
+				text: 0,
+				context: 1,
+			},
+		}),
+	])
+	.parseFilesGlob('./lib/**/*.@(ts|js)')
 
-extractor.savePotFile('./l10n/messages.pot');
+extractor.savePotFile('./l10n/messages.pot')
 
-extractor.printStats();
+extractor.printStats()
--- lib/index.ts
+++ lib/index.ts
@@ -5,21 +5,23 @@ import { getLocale } from '@nextcloud/l10n'
 const gt = new Gettext()
 
 const locale = getLocale()
-LOCALES.map(data => {
-    gt.addTranslations(data.locale, 'messages', data.json)
+LOCALES.forEach((data) => {
+	gt.addTranslations(data.locale, 'messages', data.json)
 })
 gt.setLocale(locale)
+// eslint-disable-next-line import/no-named-as-default-member
 moment.locale(locale)
 
 // Only update the locale of moment.js if it's available. Moment.js ships more locales than we
 // track in transifex, so we prefer the included translation. Always prefer our default english
 // translation.
 if (locale === 'en' || LOCALES.find(data => data.locale === locale)) {
-    moment.updateLocale(moment.locale(), {
-        relativeTime: {
-            s: gt.gettext('seconds'),
-        },
-    })
+	// eslint-disable-next-line import/no-named-as-default-member
+	moment.updateLocale(moment.locale(), {
+		relativeTime: {
+			s: gt.gettext('seconds'),
+		},
+	})
 }
 
 export default moment
